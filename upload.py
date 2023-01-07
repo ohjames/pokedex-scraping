0137b7488dbf29f7d9e2d287b405c37b902d1e18
James Nguyen <nguyenjames0404@gmail.com>
2023-01-06 20:30:03 -0800

    Used Google Sheet's API to create worksheets directly from dataframe instead of creating local csv

---
+++ upload.py
@@ -0,0 +1,16 @@
+from gen1 import data2
+import gspread
+import gspread_dataframe, gspread_formatting
+from oauth2client.service_account import ServiceAccountCredentials
+
+sheet_name = 'Ultimate Pokedex'
+
+scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets', "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(credentials)
+
+spreadsheet = client.open(sheet_name)
+worksheet1 = spreadsheet.add_worksheet(title='Gen 1 Checklist', rows='200', cols='15')
+worksheet1.clear()
+gspread_dataframe.set_with_dataframe(worksheet=worksheet1, dataframe=data2, include_index=False, include_column_header=True, resize=True)
