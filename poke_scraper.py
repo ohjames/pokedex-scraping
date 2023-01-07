143cc11b0e4dd5fb9a7143a76942e60b6a5307eb
James Nguyen <nguyenjames0404@gmail.com>
2023-01-06 22:10:44 -0800

    Created poke scraper class and updated upload.py with proper connections

---
+++ poke_scraper.py
@@ -0,0 +1,59 @@
+url = "https://www.serebii.net/pokemon/gen3pokemon.shtml"
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+class PokeScraper():
+    def __init__(self, url):
+        self.url = url
+
+    def get_header_info(self):
+        response = requests.get(self.url)
+        soup = BeautifulSoup(response.content, 'lxml')
+        table = soup.find('table', class_='dextable')
+        headers = []
+        for i in table.find_all('tr')[:2]:
+            for j in i:
+                headers.append(j.text)
+        headers = [i.removeprefix('\r\n\t\t') for i in headers]
+        headers = [i.removesuffix('\r\n\t\t') for i in headers]
+        headers = [i.removesuffix('\t') for i in headers]
+        headers = [i.replace('\n', '') for i in headers]
+        headers = [i for i in headers if i]
+        headers.pop(3)
+        headers.pop(4)
+        headers.pop(1)
+        return headers
+
+    def get_content_info(self):
+        response = requests.get(self.url)
+        soup = BeautifulSoup(response.content, 'lxml')
+        table = soup.find('table', class_='dextable')
+        content = []
+        for i in table.find_all('tr')[2:]:
+            for j in i:
+                content.append(j.text)
+        content = [i.removeprefix('\r\n\t\t') for i in content]
+        content = [i.removesuffix('\r\n\t\t') for i in content]
+        content = [i.replace('\n', '').replace(' ', '').replace('#', '') for i in content]
+        content = [i for i in content if i]
+        return content
+
+    def get_img_src(self):
+        response = requests.get(self.url)
+        soup = BeautifulSoup(response.content, 'lxml')
+        table = soup.find('table', class_='dextable')
+        src = [i for i in table.find_all('img')]
+        images = [("=IMAGE(\"" + "https://www.serebii.net"+element['src']+ "\")") for element in src if '.gif' not in element['src']]
+        return images
+
+    def create_df(self):
+        data1 = pd.DataFrame(self.get_content_info())
+        data1 = pd.DataFrame(data1[0].values.reshape(-1,9), columns=self.get_header_info())
+        data1.insert(2, "Pic", self.get_img_src())
+        data1.insert(loc = 0,column = 'Count',value = '')
+        return data1
+
+poke_list = PokeScraper(url)
+df = poke_list.create_df()
--- upload.py
+++ upload.py
@@ -1,4 +1,4 @@
-from gen1 import data2
+from poke_scraper import df
 import gspread
 import gspread_dataframe, gspread_formatting
 from oauth2client.service_account import ServiceAccountCredentials
@@ -11,6 +11,6 @@ credentials = ServiceAccountCredentials.from_json_keyfile_name('client_secret.js
 client = gspread.authorize(credentials)
 
 spreadsheet = client.open(sheet_name)
-worksheet1 = spreadsheet.add_worksheet(title='Gen 1 Checklist', rows='200', cols='15')
+worksheet1 = spreadsheet.add_worksheet(title='Gen 3 Checklist', rows='200', cols='15')
 worksheet1.clear()
-gspread_dataframe.set_with_dataframe(worksheet=worksheet1, dataframe=data2, include_index=False, include_column_header=True, resize=True)
+gspread_dataframe.set_with_dataframe(worksheet=worksheet1, dataframe=df, include_index=False, include_column_header=True, resize=True)
