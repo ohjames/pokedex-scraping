169a82cd0bfd442f4df19af3085bbc4e57540fe4
James Nguyen <nguyenjames0404@gmail.com>
2023-01-09 23:13:26 -0800

    Commented out master pokedex list, and working on regional forms

--- upload.py
+++ upload.py
@@ -11,10 +11,17 @@ credentials = ServiceAccountCredentials.from_json_keyfile_name('client_secret.js
 client = gspread.authorize(credentials)
 
 spreadsheet = client.open(sheet_name)
-for gen_number in range(0, 10):
-    url = f"https://www.serebii.net/pokemon/gen{gen_number}pokemon.shtml"
-    poke_list = PokeScraper(url)
-    df = poke_list.create_df()
-    worksheet1 = spreadsheet.add_worksheet(title=f'Gen {gen_number} Checklist', rows='200', cols='15')
-    worksheet1.clear()
-    gspread_dataframe.set_with_dataframe(worksheet=worksheet1, dataframe=df, include_index=False, include_column_header=True, resize=True)
+# for gen_number in range(0, 10):
+#     url = f"https://www.serebii.net/pokemon/gen{gen_number}pokemon.shtml"
+#     poke_list = PokeScraper(url)
+#     df = poke_list.create_gen_df()
+#     worksheet1 = spreadsheet.add_worksheet(title=f'Gen {gen_number} Checklist', rows='200', cols='15')
+#     worksheet1.clear()
+#     gspread_dataframe.set_with_dataframe(worksheet=worksheet1, dataframe=df, include_index=False, include_column_header=True, resize=True)
+
+url = 'https://www.serebii.net/swordshield/galarianforms.shtml'
+poke_list = PokeScraper(url)
+df = poke_list.create_region_df()
+worksheet1 = spreadsheet.add_worksheet(title=f'Galarian Form Checklist', rows='200', cols='15')
+worksheet1.clear()
+gspread_dataframe.set_with_dataframe(worksheet=worksheet1, dataframe=df, include_index=False, include_column_header=True, resize=True)
