2979d41631d56241c6cb5accf50f420fdc178524
James Nguyen <nguyenjames0404@gmail.com>
2023-03-07 12:42:18 -0800

    Fixed the regional issues, but missing format for hisuian form

--- poke_scraper.py
+++ poke_scraper.py
@@ -2,15 +2,16 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import re
 
 class PokeScraper():
     def __init__(self, url):
         self.url = url
 
-    def get_header_info(self):
+    def get_gen_header_info(self):
         response = requests.get(self.url)
         soup = BeautifulSoup(response.content, 'lxml')
-        table = soup.find('table', class_='dextable')
+        table = soup.find('table', {'class': 'dextable'})
         headers = []
         for i in table.find_all('tr')[:2]:
             for j in i:
@@ -25,10 +26,10 @@ class PokeScraper():
         headers.pop(1)
         return headers
 
-    def get_content_info(self):
+    def get_gen_content_info(self):
         response = requests.get(self.url)
         soup = BeautifulSoup(response.content, 'lxml')
-        table = soup.find('table', class_='dextable')
+        table = soup.find('table', {'class': 'dextable'})
         content = []
         for i in table.find_all('tr')[2:]:
             for j in i:
@@ -42,14 +43,75 @@ class PokeScraper():
     def get_img_src(self):
         response = requests.get(self.url)
         soup = BeautifulSoup(response.content, 'lxml')
-        table = soup.find('table', class_='dextable')
+        table = soup.find('table', {'class': 'dextable'})
         src = [i for i in table.find_all('img')]
         images = [("=IMAGE(\"" + "https://www.serebii.net"+element['src']+ "\")") for element in src if '.gif' not in element['src']]
         return images
 
-    def create_df(self):
-        poke_df = pd.DataFrame(self.get_content_info())
-        poke_df = pd.DataFrame(poke_df[0].values.reshape(-1,9), columns=self.get_header_info())
+    def create_gen_df(self):
+        poke_df = pd.DataFrame(self.get_gen_content_info())
+        poke_df = pd.DataFrame(poke_df[0].values.reshape(-1,9), columns=self.get_gen_header_info())
         poke_df.insert(2, "Pic", self.get_img_src())
         poke_df.insert(loc = 0,column = 'Count',value = '')
         return poke_df
+
+    def get_region_header_info(self):
+        response = requests.get(self.url)
+        soup = BeautifulSoup(response.content, 'lxml')
+        table = soup.find('table', {'class': 'tab'})
+        headers = []
+        for i in table.find_all('tr')[:2]:
+            for j in i:
+                headers.append(j.text)
+        headers = [i.removeprefix('\r\n\t\t') for i in headers]
+        headers = [i.removesuffix('\r\n\t\t') for i in headers]
+        headers = [i.removesuffix('\t') for i in headers]
+        headers = [i.replace('\n', '') for i in headers]
+        headers = [i for i in headers if i]
+        headers.pop(1)
+        headers.pop(3)
+        headers.pop(3)
+        return headers
+
+    def get_region_content_info(self):
+        response = requests.get(self.url)
+        soup = BeautifulSoup(response.content, 'lxml')
+        table = soup.find('table', {'class': 'tab'})
+        content = []
+        for i in table.find_all('tr')[2:]:
+            for j in i:
+                content.append(j.text)
+        content = [i.removeprefix('\r\n\t\t') for i in content]
+        content = [i.removesuffix('\r\n\t\t') for i in content]
+        content = [i.replace('\n', '').replace(' ', '').replace('#', '') for i in content]
+        content = [i for i in content if i]
+
+        # Removes all the Japanese characters
+        loweralphabets="abcdefghijklmnopqrstuvwxyz"
+        upperalphabets="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        numbers="0123456789"
+        desired_strings = loweralphabets + upperalphabets + numbers
+        contents = []
+        for i in content:
+            a = ""
+            for j in i:
+                if j in desired_strings:
+                    a += j
+            contents.append(a)
+        contents = [i for i in contents if i]
+        return contents
+    
+    def get_region_img_src(self):
+        response = requests.get(self.url)
+        soup = BeautifulSoup(response.content, 'lxml')
+        table = soup.find('table', {'class': 'tab'})
+        src = [i for i in table.find_all('img')]
+        images = [("=IMAGE(\"" + "https://www.serebii.net"+element['src']+ "\")") for element in src if '.gif' not in element['src']]
+        return images
+
+    def create_region_df(self):
+        region_df = pd.DataFrame(self.get_region_content_info())
+        region_df = pd.DataFrame(region_df[0].values.reshape(-1, 9), columns=self.get_region_header_info())
+        region_df.insert(2, "Pic", self.get_region_img_src())
+        region_df.insert(loc = 0,column = 'Count',value = '')
+        return region_df
--- upload.py
+++ upload.py
@@ -11,17 +11,22 @@ credentials = ServiceAccountCredentials.from_json_keyfile_name('client_secret.js
 client = gspread.authorize(credentials)
 
 spreadsheet = client.open(sheet_name)
-# for gen_number in range(0, 10):
-#     url = f"https://www.serebii.net/pokemon/gen{gen_number}pokemon.shtml"
-#     poke_list = PokeScraper(url)
-#     df = poke_list.create_gen_df()
-#     worksheet1 = spreadsheet.add_worksheet(title=f'Gen {gen_number} Checklist', rows='200', cols='15')
-#     worksheet1.clear()
-#     gspread_dataframe.set_with_dataframe(worksheet=worksheet1, dataframe=df, include_index=False, include_column_header=True, resize=True)
 
-url = 'https://www.serebii.net/swordshield/galarianforms.shtml'
-poke_list = PokeScraper(url)
-df = poke_list.create_region_df()
-worksheet1 = spreadsheet.add_worksheet(title=f'Galarian Form Checklist', rows='200', cols='15')
-worksheet1.clear()
-gspread_dataframe.set_with_dataframe(worksheet=worksheet1, dataframe=df, include_index=False, include_column_header=True, resize=True)
+# For new Pokemon from each generation
+for gen_number in range(1, 10):
+    url = f"https://www.serebii.net/pokemon/gen{gen_number}pokemon.shtml"
+    poke_list = PokeScraper(url)
+    df = poke_list.create_gen_df()
+    worksheet1 = spreadsheet.add_worksheet(title=f'Gen {gen_number} Checklist', rows='200', cols='15')
+    worksheet1.clear()
+    gspread_dataframe.set_with_dataframe(worksheet=worksheet1, dataframe=df, include_index=False, include_column_header=True, resize=True)
+
+# For Pokemon regional forms 
+regions = {'sunmoon': 'alola', 'swordshield': 'galarian', 'scarletviolet': 'paldean'}
+for key, value in regions.items():
+    url = f"https://serebii.net/{key}/{value}forms.shtml"
+    poke_list = PokeScraper(url)
+    df = poke_list.create_region_df()
+    worksheet1 = spreadsheet.add_worksheet(title=f'{value.capitalize()} Form Checklist', rows='200', cols='15')
+    worksheet1.clear()
+    gspread_dataframe.set_with_dataframe(worksheet=worksheet1, dataframe=df, include_index=False, include_column_header=True, resize=True)
